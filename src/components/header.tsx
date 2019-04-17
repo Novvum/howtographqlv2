9b8d9d77e6f1f27a4383f4241bbb61761743b4c8
TrevHeath <trevheath7@gmail.com>
2019-04-17 10:34:08 -0700

    clearned up header component.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 import Logo from './Logo';
 
-interface Props {
-  steps: { [key: string]: Step[] };
-  siteTitle: any;
-}
+// interface Props {
+//   steps: { [key: string]: Step[] };
+//   siteTitle: any;
+// }
 
 export default ({ siteTitle }) => {
   return (
